690f42d9e4bee683fcf1afbb7449cc97617a6c5e
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2021-12-04 01:09:21 +0530

    added eq 16

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
 from pymavlink import mavutil
 import enum
- 
+from scipy.interpolate import interp1d
+import numpy as np
+
 class ServoStatus(enum.Enum):
     disabled = 0
     enabled = 1
@@ -55,9 +57,36 @@ if __name__ == '__main__':
 
     print("Servo modes set")
 
-    set_servo_pwm(s1, 1800)
-    set_servo_pwm(s2, 1800)
-    set_servo_pwm(s3, 1800)
-    set_servo_pwm(s4, 1800)
+    pwm_1 = 1800
+    pwm_3 = 1500
+    pwm_4 = 1400
 
     print("Servo PWM set")
+
+    ## Mappings for thrust to force
+    ## Used a sample thruster/motor for force calculation:
+    ## https://uav-en.tmotor.com/html/2018/u_0222/3.html
+    ## Prop size: 12*4CF
+    # 50% - 350 G
+    # 65% - 550 G
+    # 75% - 700 G
+    # 85% - 870 G
+    # 100% - 1000 G
+    x =  [1000, 1500, 1650, 1750, 1850, 2000]
+    y = [0, 350, 550, 700, 870, 1000]
+
+    # Function that maps motor PWM to Force in grams
+    pwm_to_force = interp1d(x, y, 'cubic')
+
+    ## Since motor uses 12inch prop, min. required arm length = root(2) * half (arm length) = 1.414 * 100 cm - 30 > 0
+    ## arm length = 100 cm gives ~10cm + space
+    # Arm Length (l)
+    l = arm_length = 1
+    # Drag to Thrust co-efficients of the blade (12*4CF)
+    d = drag_thrust_co_efficient = 4
+
+    left_matrix = np.array([[1, 1, 1], [-l, l, 0], [d, d, -d]])
+    right_matrix = np.array([pwm_to_force(pwm_1), pwm_to_force(pwm_3), pwm_to_force(pwm_4)])
+    force_matrix = np.dot(left_matrix,right_matrix)
+
+    print(force_matrix)
